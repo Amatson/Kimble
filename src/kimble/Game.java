4709f4268907888477d733d7d1603a3bcc1cd6c7
Matti Ojala <matti.ojala@iki.fi>
2019-01-10 18:46:41 +0200

    Added Naks functionality, done some test cases. Looking good!

--- src/kimble/Game.java
+++ src/kimble/Game.java
@@ -1,6 +1,8 @@
 package kimble;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 /*
  * This class takes care of the main functionality of the game. It initializes the gameboard, sets players and 
@@ -12,6 +14,7 @@ public class Game {
 	
 	private Board board;
 	private HashMap<Colors, Player> players;
+	private int dice = 0;
 	
 	
 	/*
@@ -48,7 +51,7 @@ public class Game {
 		this.players.put(Colors.GREEN, greenPlayer);
 		this.players.put(Colors.YELLOW, yellowPlayer);
 		
-		
+		naks();
 	}
 
 
@@ -72,6 +75,42 @@ public class Game {
 		this.board = board;
 	}
 
+	
+	/*
+	 * The next method gets a random dice naks which is not a throw.
+	 * In Kimble probabilities of dice return values are not distributed evenly.
+	 * The same dice number is returned with about 11% probability.
+	 * The opposite dice number is returned with about 24% probability.
+	 * Other dice values share the rest of the probabilities (65%)
+	 * 
+	 * However, as good players know, technique and skills can alter the 
+	 * probabilities but this is impossible to simulate programmatically.
+	 */
+	public int naks() {
+		if(this.dice == 0) {
+			Random rand = new Random();
+			this.dice = rand.nextInt(6)+1;
+			return this.dice;
+		}
+		else {
+			Random rand = new Random();
+			int P = rand.nextInt(1000);
+//			if(P >= 0 && P < 110)
+			// This does nothing as it is the probability for unchanging dice number
+			if(P >= 110 && P < 350) {
+				this.dice = 7 - this.dice;
+			}
+			else { // 350 < P < 999
+				ArrayList<Integer> nums = new ArrayList<Integer>();
+				for(int i = 1; i < 7; i++) {
+					if(i != this.dice && i != (7-this.dice))
+						nums.add(i);
+				}
+				this.dice = nums.get(rand.nextInt(4));
+			}
+			return this.dice;
+		}
+	}
 
 
 }
--- src/kimble/KimbleTest.java
+++ src/kimble/KimbleTest.java
@@ -1,6 +1,7 @@
 package kimble;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,10 +9,18 @@ public class KimbleTest {
 	
 	@Test
 	public void testGameInitialization() {
+		System.out.println("Hello World! This is Kimble testing!");
 		Game game = new Game(4);
 		assertEquals(4, game.getBoard().getBoardSize());
 		assertEquals(4, game.getPlayerWithColor(Colors.RED).countHomeTokens());
 		assertEquals(Field.Type.HOME, game.getPlayerWithColor(Colors.BLUE).getHomeToken().getField().getType());
+		for(int i = 0; i < 10; i++) {
+			int naks = game.naks();
+			assertTrue("Error, naks is too high", naks < 7);
+			assertTrue("Error, naks is too low", naks > 0);
+		}
+	
+		
 	}
 
 }
